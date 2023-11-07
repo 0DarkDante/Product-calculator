935405ef394766be09d0c33bf50f2d117eed1ed5
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-07 12:06:52 +0200

    the program calculates the sum of all purchases

--- script.js
+++ script.js
@@ -5,33 +5,48 @@ let add    = document.querySelector('#add');
 let table  = document.querySelector('#table');
 let total  = document.querySelector('#total');
 
-function createCell(row, value, className) {
+function createCell(value, className) {
   let cell = document.createElement('td');
   cell.textContent = value;
   cell.classList.add(className);
-  row.appendChild(cell);
+  return cell;
+}
+
+function recountTotal() {
+	let costs = table.querySelectorAll('.cost');
+  
+	if (costs) {
+    let sum = 0;
+
+    for(let cost of costs) {
+      cost = Number(cost.textContent);
+      sum += cost;
+    }
+    total.textContent = sum;
+	}
 }
 
 add.addEventListener('click', function() {
   let tr = document.createElement('tr');
   
-  createCell(tr, name.value, 'name');
-  createCell(tr, price.value, 'price');
-  createCell(tr, amount.value, 'amount');
+  tr.appendChild(createCell(name.value, 'name'));
+  tr.appendChild(createCell(price.value, 'price'));
+  tr.appendChild(createCell(amount.value, 'amount'));
   let cost = price.value * amount.value;
-  createCell(tr, cost, 'cost');
+  tr.appendChild(createCell(cost, 'cost'));
   
   let removeCell = document.createElement('td');
   removeCell.textContent = 'удалить';
   removeCell.classList.add('remove');
   removeCell.addEventListener('click', function() {
     tr.remove();
-    updateTotal();
+    recountTotal();
   });
   tr.appendChild(removeCell);
   
   table.appendChild(tr);
-  updateTotal();
+  recountTotal();
 });
+
 
 
