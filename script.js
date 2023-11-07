09784f06cb1412eb7530959c26fced95f9fe8e41
Dark Dante <92054470+0DarkDante@users.noreply.github.com>
2023-11-07 11:19:34 +0200

    Added function to add items and remove object

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ let amount = document.querySelector('#amount');
 let add    = document.querySelector('#add');
 let table  = document.querySelector('#table');
 let total  = document.querySelector('#total');
+
+function createCell(row, value, className) {
+  let cell = document.createElement('td');
+  cell.textContent = value;
+  cell.classList.add(className);
+  row.appendChild(cell);
+}
+
+add.addEventListener('click', function() {
+  let tr = document.createElement('tr');
+  
+  createCell(tr, name.value, 'name');
+  createCell(tr, price.value, 'price');
+  createCell(tr, amount.value, 'amount');
+  let cost = price.value * amount.value;
+  createCell(tr, cost, 'cost');
+  
+  let removeCell = document.createElement('td');
+  removeCell.textContent = 'удалить';
+  removeCell.classList.add('remove');
+  removeCell.addEventListener('click', function() {
+    tr.remove();
+    updateTotal();
+  });
+  tr.appendChild(removeCell);
+  
+  table.appendChild(tr);
+  updateTotal();
+});
+
+
